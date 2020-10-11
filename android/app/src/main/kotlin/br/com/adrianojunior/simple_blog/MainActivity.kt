eb9861557b66f058a2ca14b8d752f22bad2ffe83
Adriano <developer.adrianoscjr@gmail.com>
2020-10-11 04:26:53 -0300

    Base para a produção do app

---
+++ android/app/src/main/kotlin/br/com/adrianojunior/simple_blog/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.adrianojunior.simple_blog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const accentColor = Color(0xffF44336);
+
+const primaryColor = Color(0xff2196F3);
+
+const primaryColorDark = Color(0xff0069C0);
+
+const String userName = "Usuário";
